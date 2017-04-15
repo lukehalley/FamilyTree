24a1c8f8777e7e3b5f8ef5c5dce3d30d09d9719e
Luke Halley <luke123halley@gmail.com>
2017-04-15 11:42:04 +0800

    Upload
    Linking people to parents complete

---
+++ src/controllers/Main.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import utils.Import;
+
+public class Main {
+
+	public static void main(String args[]) throws Exception {
+
+		Import.InitialImport();
+		Import.LinkParents();
+
+	}
+
+}

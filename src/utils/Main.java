719f7230b7330a716c2535f7b14fd6e582300b01
Luke Halley <luke123halley@gmail.com>
2017-05-23 08:45:02 +0800

    GUI

---
+++ src/utils/Main.java
@@ -0,0 +1,59 @@
+package utils;
+
+import java.awt.BorderLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+@SuppressWarnings("serial")
+public class Main extends JPanel {
+
+	JList<String> list;
+
+	DefaultListModel<String> model;
+
+	public Main() {
+		setLayout(new BorderLayout());
+
+		model = new DefaultListModel<String>();
+		list = new JList<String>(model);
+		JScrollPane pane = new JScrollPane(list);
+
+		for (String key : Import.FamilyMap.keySet()) {
+			model.addElement(key);
+		}
+
+		JTextArea personDetails = new JTextArea();
+		personDetails.setText("Please select a person from the list on the left to view their family tree");
+
+		list.addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				if (e.getValueIsAdjusting()) {
+					personDetails.setText(Import.FamilyMap.get(list.getSelectedValue()).toString());
+				}
+			}
+
+		});
+
+		add(pane, BorderLayout.WEST);
+		add(personDetails, BorderLayout.CENTER);
+	}
+
+	public static void main(String s[]) {
+		Import.InitialImport();
+		Import.LinkParents();
+		JFrame frame = new JFrame("Family Tree");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(new Main());
+		frame.setSize(1000, 1000);
+		frame.setVisible(true);
+	}
+
+}
